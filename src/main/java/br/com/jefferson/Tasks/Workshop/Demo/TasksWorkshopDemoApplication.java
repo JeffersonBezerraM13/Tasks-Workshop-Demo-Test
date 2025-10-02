64b81b6ddfaced169b2788d83837b525d0fbedba
Jefferson Bezerra <jeffebezerram13@gmail.com>
2025-10-02 12:54:56 -0300

    first commit

---
+++ src/main/java/br/com/jefferson/Tasks/Workshop/Demo/TasksWorkshopDemoApplication.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.Tasks.Workshop.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksWorkshopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksWorkshopDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jefferson/Tasks/Workshop/Demo/TasksWorkshopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.Tasks.Workshop.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksWorkshopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
